2a1407ead2a0d340974e62ef7d3cf756189adb78
PhamHuyThien <sync.mail.pc@gmail.com>
2022-03-04 19:51:25 +0700

    update d.util to env

--- utils/db.util.js
+++ utils/db.util.js
@@ -1,7 +1,14 @@
 const mongoose = require("mongoose");
 
-const db = (callback = (err) => { }) => {
-    mongoose.connect("mongodb://localhost:27017/game-demo", (err) => callback(err));
-}
+const MDB_HOST = process.env.MDB_HOST || "localhost";
+const MDB_PORT = process.env.MDB_PORT || 27017;
+const MDB_DB = process.env.MDB_DB || "game-demo";
+const MDB_USER = process.env.MDB_USER || "";
+const MDB_PASS = process.env.MDB_PASS || "";
+
+const AUTH = MDB_USER != "" && MDB_PASS != "" ? MDB_USER + ":" + MDB_PASS + "@" : "";
+const URL = `mongodb://${AUTH}${MDB_HOST}:${MDB_PORT}/${MDB_DB}`;
+
+const db = (callback = (err) => { }) => mongoose.connect(URL, err => callback(err));
 
 module.exports = db;
